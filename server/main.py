4d1d90d85c53fe13ed918a1e8bafdef5a0e2d232
Its-Tanay <tantiw1910@gmail.com>
2024-08-26 02:00:22 +0530

    Build fix 3

--- server/main.py
+++ server/main.py
@@ -3,10 +3,14 @@ from flask import g
 from app.utils.config import load_config
 
 if __name__ == "__main__":
+
     app = create_app()
     
     app.config.update(load_config())
 
     with app.app_context():
-        g.db_url = app.config['DB_URL']
         app.run()
+
+else:
+    gunicorn_app = create_app()
+    gunicorn_app.config.update(load_config())
